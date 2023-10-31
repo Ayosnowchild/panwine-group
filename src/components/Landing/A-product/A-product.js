cfba18cb115da7d6104a19dcb309c6ee5688fe62
awodiranj@gmail.com <Harbideymi@4178>
2023-10-31 16:18:13 +0100

    landing

--- src/components/Landing/A-product/A-product.js
+++ src/components/Landing/A-product/A-product.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import './A-product.css'
-import Addcart from '../Addcart/addcart'
+import React from "react";
+import "./A-product.css";
+import Addcart from "../Addcart/addcart";
 
 function Products(props) {
   return (
-    <div className='productp'>
-        <div className='prod-cont'>
-        <img src={props.img} className='prod-img'/>
-        <p className='big-name'>{props.prodname}</p>
-        <p className='small-name'>{props.smallname}</p>
-        <div className='addcart-cont'>
-            <Addcart/>
-            <p className='pro-price'>#12,400.00</p>
+    <div className="productp">
+      <div className="prod-cont">
+        <img src={props.img} alt="products" className="prod-img" />
+        <p className="big-name">{props.prodname}</p>
+        <p className="small-name">{props.smallname}</p>
+        <div className="addcart-cont">
+          <Addcart />
+          <p className="pro-price">#12,400.00</p>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
