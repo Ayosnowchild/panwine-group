47f8474dcace08495823e4ab777d1755a533880f
AzeezMutiu <mr9cguymd@gmail.com>
2023-11-07 03:34:18 -0800

    azeez commit

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Landing from "../pages/Landing";
-import Password from "../pages/Password";
-import SignUp from "../pages/SignUp";
+import Password from "../pages/Password-reset";
+import SignUp from "../pages/Sign-up";
 import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
@@ -15,7 +15,7 @@ export const router = createBrowserRouter([
       { path: "/Login", element: <Login /> },
       {
         path: "/SignUp",
-        element: <SignUp goog={<img src="./goog.png" alt={""} />} />,
+        element: <SignUp />,
       },
       { path: "/Password", element: <Password /> },
     ],
